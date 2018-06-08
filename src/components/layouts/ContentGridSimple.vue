f65b7b673ea50e18a5dd0d2b0dca9bf6b7ad8016
it-coral <coral1212@yahoo.com>
2018-06-08 08:01:10 -0400

    update header with new image

--- src/components/layouts/ContentGridSimple.vue
+++ src/components/layouts/ContentGridSimple.vue
@@ -1,7 +1,14 @@
 <template>
     <div>
         <header>
-            <h1>Fortnite Boosted</h1>
+            <div class="menu">
+                <span class="menu-item" v-for="item in menus" :key="item.value">
+                    <a v-bind:href="item.link" :class="item.value != 'LOGIN' ? 'menu-link' : 'menu-link login-menu-link'">{{item.value}}</a>
+                </span>
+            </div>
+            <div class="menu">
+                <img class="header-logo" v-bind:src="logoPath" />
+            </div>
         </header>
         <main>
             <div id="steps-bar">
@@ -27,6 +34,7 @@
 <script>
 import { Button, ButtonGroup, Steps, Step } from 'element-ui';
 import LoginBar from '@/components/LoginBar.vue';
+import logoImage from '@/assets/logo.png';
 
 export default {
   name: 'ContentGrid',
@@ -36,5 +44,54 @@ export default {
   components: {
     Button, ButtonGroup, LoginBar, Steps, Step,
   },
+  data() {
+    return {
+      menus: [
+        {
+          link: '/about',
+          value: 'ABOUT',
+        },
+        {
+          link: '/contact',
+          value: 'CONTACT',
+        },
+        {
+          link: '/faq',
+          value: 'FAQ',
+        },
+        {
+          link: '/login',
+          value: 'LOGIN',
+        },
+      ],
+      logoPath: logoImage,
+    };
+  },
 };
 </script>
+
+<style lang="less">
+    .menu {
+        text-align: center;
+        margin: 3em 0;
+    }
+
+    .menu-item {
+        margin: 0 2em;
+    }
+
+    .menu-link {
+        color: white;
+        text-decoration: none;
+    }
+
+    .login-menu-link {
+        background: #00b4ff;
+        padding: .5em;
+    }
+
+    .header-logo {
+        width: 350px;
+        margin-top: 2em;
+    }
+</style>
